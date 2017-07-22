dd89dfe2df84508c0ddf094a54160e430900ccb0
Adedamola Shomoye <dshomoye@gmail.com>
2017-07-21 21:33:33 -0400

    creating and checking a ticket for a passenger now also requires token for the passenger/user

--- dabi/dabi/resources.py
+++ dabi/dabi/resources.py
@@ -3,7 +3,6 @@ from models import *
 from . import pwd_context
 import datetime
 import jwt
-from functools import wraps
 
 
 def check_token(token,hashp):
@@ -26,7 +25,9 @@ class Token(Resource):
         sp.add_argument("username",required=True)
         sp.add_argument("password",required=True)
         args = sp.parse_args()
+
         pauth = get_user_auth(args["username"])
+        
         if pwd_context.verify(args["password"], pauth):
             token = jwt.encode({"username":args["username"],
                  'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
@@ -43,8 +44,10 @@ class Passenger(Resource):
         sp = reqparse.RequestParser()
         sp.add_argument("token", required=True)
         args = sp.parse_args()
+
         if not check_token(args["token"],hashp):
             return "Invalid token",403
+
         p = get_passenger_info(passenger_id)
         if p:
             p = dict(
@@ -65,8 +68,10 @@ class Passenger(Resource):
         sp.add_argument("username",required=True)
         sp.add_argument("password",required=True)
         args = sp.parse_args()
+
         if "address" not in args:
             args["address"] = None
+        
         hashp = pwd_context.hash(args["password"])
         p = create_passenger(
             lname = args['last_name'],
@@ -87,8 +92,15 @@ class Ticket(Resource):
             sp.add_argument(a,required=True)
         sp.add_argument("return_date_time")
         sp.add_argument("return_train")
+        sp.add_argument("token")
+
 
         args = sp.parse_args()
+
+        if not check_token(args["token"], 
+        get_passenger_auth(args["passenger_id"])):
+            return "Invalid token", 403
+
         if(args["return_date_time"] and args["return_train"]):
             round_trip = 1
             return_date_time = args["return_date_time"]
@@ -112,8 +124,11 @@ class Ticket(Resource):
         return t
     
     def get(self,ticket_number):
+        sp = reqparse.RequestParser()
+        sp.add_argument("token",required=True)
+        args = sp.parse_args()
         ti = get_ticket_record(ticket_number)
-        return dict(
+        result = dict(
             start_station = ti[1],
             end_station = ti[2],
             train_number = ti[3],
@@ -124,6 +139,10 @@ class Ticket(Resource):
             return_date_time = ti[8],
             fare=ti[9]
         )
+        if not check_token(args["token"], 
+        get_passenger_auth(result["passenger_id"])):
+            return "Invalid token", 403
+        return result
 
             
             
